3ed1bbe408ed7c76d9cacf5f1f024d35c7e18c24
Franco-Corvalan <129094317+Franco-Corvalan@users.noreply.github.com>
2024-07-23 00:04:29 -0300

    retoques

--- components/Game.tsx
+++ components/Game.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { SafeAreaView, View, StyleSheet } from 'react-native';
 import { PanGestureHandler } from 'react-native-gesture-handler';
 import { Colors } from '../styles/colores';
+import { Coordenada, Direction, GestureEventType } from '../types/types';
 import Header from './Header';
 
 const SnakeInicio = [{x:5,y:5}];
@@ -11,8 +12,44 @@ const MoveIntervaloTiempo = 50;
 const ScoreContador = 10;
 
 export default function Game(): JSX.Element {
+    const [direction , setDirection] = React.useState<Direction>(Direction.Right);
+    const [snake,setSnake] = React.useState<Coordenada[]>(SnakeInicio);
+    const [food,setFood] = React.useState<Coordenada>(FoodInicio);
+    const [isGameOver, setIsGameOver] = React.useState<boolean>(false);
+
+    React.useEffect(()=>{
+       if(!isGameOver){
+
+       }
+    }, [isGameOver]);
+
+   const moveSnake =()=>{
+    const snakeCabeza = snake[0];
+   };
+
+    const handlerGesture = (event: GestureEventType) => {
+        // console.log(event.nativeEvent);
+        const {translationX, translationY} = event.nativeEvent;
+        // console.log(translationX,translationY); 
+        console.log(direction);
+
+        if(Math.abs(translationX) > Math.abs(translationY)){
+            if (translationX > 0){
+                setDirection(Direction.Right);
+            }else {
+                setDirection(Direction.Left);
+            }
+        }else {
+            if(translationY > 0){
+                setDirection(Direction.Down);
+            }else{
+                setDirection(Direction.Up);
+            }
+        }
+    };
+
     return (
-        <PanGestureHandler>
+        <PanGestureHandler onGestureEvent={handlerGesture}>
             <SafeAreaView style={styles.container}>
                 {/* <Header>
                 </Header> */}
---
+++ types/types.ts
@@ -0,0 +1,15 @@
+export enum Direction {
+    Right,
+    Up,
+    Left,
+    Down
+}
+
+export interface GestureEventType{
+    nativeEvent:{translationX: number; translationY:number};
+}
+
+export interface Coordenada{
+    x:number;
+    y:number;
+}
